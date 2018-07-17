97295538d6095eeaacc20be65983e6e8072d5c32
Tsvetozar Bonev <tsbonev@gmail.com>
2018-07-17 10:49:53 +0300

    Add functions as parameters

--- src/main/java/demo/Reflection.kt
+++ src/main/java/demo/Reflection.kt
@@ -14,16 +14,26 @@ fun printStuffWithSpaces(printMe: String){
     println()
 }
 
-fun useFunction(parameter: String, func: (String) -> Unit){
-    func(parameter)
-}
+fun useFunction(parameter: String, func: (String) -> Unit) = func(parameter)
+
 
 val value: KClass<Int> = Int::class
+
+val valIsFun: (String) -> Unit = ::printStuff
+
+val valIsLambdaFun = { s: String ->
+    println(s)
+    println("-----")
+}
 
 fun main(args: Array<String>) {
 
     useFunction("Print me", ::printStuff)
     useFunction("Print me too", ::printStuffWithSpaces)
+
+    useFunction("Print me with a value", valIsFun)
+
+    useFunction("Print me with an underscore", valIsLambdaFun)
 
     println(value)
 
